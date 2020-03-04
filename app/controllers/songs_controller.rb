4fb1deab30515cef77520cdcf07ca4875f3d9792
github username <github email address>
2020-03-04 13:20:39 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,11 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+     # Build 3 "empty" notes that are automatically set up with the correct
+    # parameter names to be used by accepts_nested_attributes_for
+    # See:
+    # https://api.rubyonrails.org/v5.2.3/classes/ActionView/Helpers/FormHelper.html#method-i-fields_for
+    3.times { @song.notes.build }
   end
 
   def create
